0ecfcd3a2edec7ef683abd65df597c174c51176a
BAyush619 <bhayaniayush619@gmail.com>
2026-03-18 22:32:18 +0530

    starting working on Note Accordion

--- ReactNotes/src/pages/Notes.jsx
+++ ReactNotes/src/pages/Notes.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+import "../styles/pagesStyle/Notes.css"
+
 function Notes() {
+  const [openAccordian, setOpenAccordian] = useState(false);
   return (
     <>
-      <div>Notes</div>
+      <div className="NotesWrapper">
+        <div className="InputAccordian" onClick={() => setOpenAccordian(true)}>
+          Take a note....
+          <br />
+          <div>
+            {
+              openAccordian &&
+              <input type="text" placeholder="enter notes here" />
+            }
+          </div>
+        </div>
+      </div>
     </>
   )
 }
