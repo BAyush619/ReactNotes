92ad5b4011f905c0d88efbf6ba8dc8c127e6c90a
BAyush619 <bhayaniayush619@gmail.com>
2026-03-19 00:31:35 +0530

    working in Text Accordion

--- ReactNotes/src/pages/Notes.jsx
+++ ReactNotes/src/pages/Notes.jsx
@@ -1,18 +1,56 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "../styles/pagesStyle/Notes.css"
+import { BsPin } from "react-icons/bs";
 
 function Notes() {
   const [openAccordian, setOpenAccordian] = useState(false);
+  const accordionRef = useRef(null);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (accordionRef.current &&
+        !accordionRef.current.contains(event.target)) {
+        setOpenAccordian(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+  }, [])
+
   return (
     <>
       <div className="NotesWrapper">
-        <div className="InputAccordian" onClick={() => setOpenAccordian(true)}>
-          Take a note....
+        <div className={`InputAccordian ${openAccordian ? "opneAccordian" : ""}`}
+          ref={accordionRef}
+          onClick={() => setOpenAccordian(true)}>
+          {
+            openAccordian ?
+              <div className="tile-inputWrapper">
+                <div>
+                  <input type="text" className="NotesTitle" placeholder="Title" />
+                </div>
+                <div className="pin-wrpper">
+                  <BsPin />
+                  <span className="tooltip-pin">Pin note</span>
+                </div>
+              </div>
+              :
+              <div className="accordian-body">
+                Take a note....
+              </div>
+          }
+
+
           <br />
           <div>
             {
               openAccordian &&
-              <input type="text" placeholder="enter notes here" />
+              <input type="text" className="WritingNoteSection" placeholder="Take a note...." />
+              // make text are instead of input
             }
           </div>
         </div>
