498df72cc3ae223a0d1f8704069241a394d9e775
BAyush619 <bhayaniayush619@gmail.com>
2026-03-24 19:47:38 +0530

    working in DisplayNoteCard css

--- ReactNotes/src/components/DisplayNoteCard.jsx
+++ ReactNotes/src/components/DisplayNoteCard.jsx
@@ -1,4 +1,7 @@
 import "../styles/DisplayNoteCard.css";
+import { RiPushpin2Line } from "react-icons/ri";
+import { MdArchive } from "react-icons/md";
+import { MdDeleteForever } from "react-icons/md";
 
 function DisplayNoteCard({ noteItem }) {
   return (
@@ -9,6 +12,17 @@ function DisplayNoteCard({ noteItem }) {
         </div>
         <div className="NoteDiscription">
           {noteItem.description}
+        </div>
+        <div className="cardPinWrapper">
+          <RiPushpin2Line />
+        </div>
+        <div className="cardActionWrapper">
+          <div className="cardArchiveWrapper">
+            <MdArchive />
+          </div>
+          <div className="cardDeleteWrapper">
+            <MdDeleteForever />
+          </div>
         </div>
       </div>
     </>
